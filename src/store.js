e49e6817f293140d4153c7c72710f850e5369085
jihoon <hun2ya9@gmail.com>
2022-01-21 17:27:16 +0900

    feat(space, useGetSpaceByIdQuery): 공간정보 API 데이터 호출
    space의 변경점:
    - useGetSpaceByIdQuery Hook을 통하여 공간정보 API 호출
    - 현재는 데이터 안의 필요한 값들만 가져오지 않고 전체 데이터를 가져오는 방식으로 구현 하였음
    - 서버 데이터가 실시간으로 업데이트가 되지 않는 정적인 데이터라고 가정하고 구현하였음
    - 데이터가 필요한 곳에서 해당 Hook을 직접적으로 사용해도 되고, 별도의 Hook을 만들어서 View와 분리해도 상관 없음

--- src/store.js
+++ src/store.js
@@ -1,15 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
-// import { pokemonApi } from "./services/pokemon";
+import { spaceApi } from "services/space";
 
 export const store = configureStore({
   reducer: {
     // 특정 top-level slice에서 생성된 리듀서를 추가
-    // [pokemonApi.reducerPath]: pokemonApi.reducer,
+    [spaceApi.reducerPath]: spaceApi.reducer,
   },
   // 캐싱, 요청 취소, 폴링 등등 유용한 rtk-query의 기능들을 위한 api 미들웨어 추가
-  // middleware: (getDefaultMiddleware) =>
-  //   getDefaultMiddleware().concat(pokemonApi.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(spaceApi.middleware),
   devTools: process.env.NODE_ENV !== "production", //  개발자 도구 on/off
 });
 
